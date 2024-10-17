504fcaa0c52d2a6522fc67e5657634c178e8d9d3
Joao Quiziri <quizirironchi@hotmail.com>
2024-10-16 21:34:15 -0300

    Adicionando Incluir e buscar para ADM

--- Dominio/Interfaces/IAdministradorServico.cs
+++ Dominio/Interfaces/IAdministradorServico.cs
@@ -6,5 +6,8 @@ namespace MinimalAPI.Dominio.Services
     public interface IAdministradorServico
     {
         Administrador? Login(LoginDTO loginDTO);
+        Administrador Incluir(Administrador administrador);
+        Administrador? BuscaPorId(int id);
+        List<Administrador> Todos(int? pagina);
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using MinimalAPI.Dominio.ModelViews;
 using MinimalAPI.Dominio.Services;
 using MinimalAPI.Infraestrutura.Db;
 using MinimalAPI.Dominio.Entidades;
+using MinimalAPI.Dominio.Enums;
 
 #region Builder
 var builder = WebApplication.CreateBuilder(args);
@@ -40,6 +41,75 @@ app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministra
     {
         return Results.Unauthorized();
     }
+}).WithTags("Administradores");
+
+app.MapGet("/administradores", ([FromQuery] int? pagina, IAdministradorServico administradorServico) => {
+    var adms = new List<AdminstradorModelView>();
+    var administradores = administradorServico.Todos(pagina);
+    foreach(var adm in administradores)
+    {
+        adms.Add(new AdminstradorModelView{
+            Id = adm.Id,
+            Email = adm.Email,
+            Perfil = adm.Perfil
+        });
+    }
+    return Results.Ok(adms);
+}).WithTags("Administradores");
+
+app.MapGet("/administradores/{id}", ([FromRoute] int id, IAdministradorServico administradorServico) => {
+    var administrador = administradorServico.BuscaPorId(id);
+    if(administrador == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(new AdminstradorModelView{
+            Id = administrador.Id,
+            Email = administrador.Email,
+            Perfil = administrador.Perfil
+        });
+}).WithTags("Administradores");
+
+app.MapPost("/administradores", ([FromBody] AdministradorDTO administradorDTO, IAdministradorServico administradorServico) => {
+    var validacao = new ErrosDeValidacao
+    {
+        Mensagens = new List<string>()
+    };
+
+    if(string.IsNullOrEmpty(administradorDTO.Email))
+    {
+        validacao.Mensagens.Add("Email não pode ser vazio");
+    }
+
+    if(string.IsNullOrEmpty(administradorDTO.Senha))
+    {
+        validacao.Mensagens.Add("Senha não pode ser vazia");
+    }
+
+    if(administradorDTO.Perfil == null)
+    {
+        validacao.Mensagens.Add("Perfil não pode ser vazio");
+    }
+
+    if(validacao.Mensagens.Count > 0)
+    {
+        return Results.BadRequest(validacao);
+    }
+
+    var administrador = new Administrador
+    {
+        Email = administradorDTO.Email,
+        Senha = administradorDTO.Senha,
+        Perfil = administradorDTO.Perfil.ToString() ?? Perfil.Editor.ToString()
+    };
+
+    administradorServico.Incluir(administrador);
+
+    return Results.Created($"/administrador/{administrador.Id}", new AdminstradorModelView{
+            Id = administrador.Id,
+            Email = administrador.Email,
+            Perfil = administrador.Perfil
+        });
 }).WithTags("Administradores");
 #endregion
 
